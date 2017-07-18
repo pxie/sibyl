abdf83b389141c647381d8c1ba70fc2ba881be7f
Pin Xie <pin.xie@ge.com>
2017-07-18 16:29:19 +0800

    add build script, and release build

--- release/build.py
+++ release/build.py
@@ -2,16 +2,27 @@ import os
 import os.path
 import shutil
 
+# make temp build folder
+dest = os.path.join(os.getcwd(), "build")
+if os.path.exists(dest):
+    shutil.rmtree(dest)
+os.mkdir(dest)
 
-print(os.getcwd())
+# copy source code in build folder
+root_dir = os.path.join(os.getcwd(), "../")
+shutil.copytree(os.path.join(root_dir, "sibyl"), os.path.join(dest, "sibyl"))
+shutil.copy(os.path.join(root_dir, "config.json"), dest)
 
-for filename in os.listdir(os.path.join(os.getcwd(), "../")):
-    if filename.endswith(".pyc"):
-        print(filename)
-        os.remove(filename)
-    else:
-        print("not remove %s", filename)
+for subdir, dirs, files in os.walk(dest):
+    for f in files:
+        if f.endswith(".pyc"):
+            # print(f)
+            os.remove(os.path.join(subdir, f))
 
-shutil.make_archive("./sibyl.release.zip",
-                    "zip",
-                    os.path.join(os.getcwd(), "../"))
+# remove zip build
+if os.path.exists("./sibyl.release.zip"):
+    os.remove("./sibyl.release.zip")
+
+print("generate release build: sibyl.release.zip")
+shutil.make_archive("./sibyl.release", "zip", dest)
+shutil.rmtree(dest)
